5a0db5c75995b8831f538a08ea301e9db213cd5c
Luis Murrugarra <luismurrugarra17@gmail.com>
2023-09-14 00:28:10 -0500

    Add Mail Module

---
+++ src/mails/mails.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+import { MailsService } from './mails.service';
+@Controller('mails')
+export class MailsController {
+  constructor(private readonly mailsService: MailsService) {}
+}
